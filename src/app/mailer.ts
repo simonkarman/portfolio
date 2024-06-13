ea6a6f0b3ba7b32fb2cd59cefd531e3fcd14b036
Simon Karman <mail@simonkarman.nl>
2024-06-13 17:35:18 +0200

    Added mailer information

--- src/app/mailer.ts
+++ src/app/mailer.ts
@@ -1,3 +1,12 @@
+/**
+ * The mailer module is responsible for sending emails to the user. It uses the mailer API defined in the simonkarman-serverless BitBucket project to
+ *  send emails. The mailer implementation can be found at: `https://bitbucket.org/simonkarman/simonkarman-serverless/src/master/`.
+ *
+ * In the dev environment, the mailer module will not send emails but instead return a fake response. This is to prevent sending emails while testing
+ *  out functionality.
+ *
+ * It is hosted on AWS Lambda and is accessible through the following base URL.
+ */
 const mailerBaseUrl = 'https://2ct3ol18ee.execute-api.eu-west-1.amazonaws.com/dev';
 // eslint-disable-next-line no-process-env
 const isDev = process.env.NODE_ENV !== 'production';
