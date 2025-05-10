e868e4e4b2487abce679adba13345488718050ee
Simon Karman <mail@simonkarman.nl>
2025-05-10 09:13:58 +0000

    Validate that no two projects have the same slug

--- src/utils/projects/get-all-projects.ts
+++ src/utils/projects/get-all-projects.ts
@@ -5,6 +5,23 @@ import { getProviders } from './providers';
 
 export async function getAllProjects(): Promise<Project[]> {
   const providers = await getProviders();
-  const projects = await Promise.all(Object.values(providers).map(provider => provider.getProjects()));
-  return projects.flatMap(p => p).sort((a, b) => b.date.localeCompare(a.date));
+  const projectsByProvider = await Promise.all(Object.values(providers).map(provider => provider.getProjects()));
+  const projects = projectsByProvider.flatMap(p => p).sort((a, b) => b.date.localeCompare(a.date));
+
+  // Validate that no two projects have the same slug
+  const projectsBySlug = projects.reduce((acc, project) => {
+    if (!acc[project.slug]) {
+      acc[project.slug] = [project.providerName];
+    } else {
+      acc[project.slug].push(project.providerName);
+    }
+    return acc;
+  }, {} as Record<string, string[]>);
+  const duplicateSlugs = Object.entries(projectsBySlug).filter(([_, providers]) => providers.length > 1);
+  if (duplicateSlugs.length > 0) {
+    console.error('Duplicate slugs', duplicateSlugs);
+    throw new Error(`${duplicateSlugs.length} duplicate slug(s) found`);
+  }
+
+  return projects;
 }
