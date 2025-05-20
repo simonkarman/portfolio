c5efa4b050e121cfc0bd317b1e03aae30f53228c
Simon Karman <mail@simonkarman.nl>
2025-05-20 16:44:55 +0200

    Disable all providers except contentful for now and removed the obsolete contentful-to-markdown ulity

--- src/projects/providers.ts
+++ src/projects/providers.ts
@@ -2,15 +2,12 @@
 
 import { Provider } from './provider';
 import { ContentfulProvider } from '@/projects/contentful-provider/contentful-provider';
-import { GithubProvider } from '@/projects/github-provider/github-provider';
-import { MarkdownProvider } from './markdown-provider/markdown-provider';
-import { MdxProvider } from '@/projects/mdx-provider/mdx-provider';
 
 const providers = {
   'contentful': new ContentfulProvider(),
-  'github': new GithubProvider(),
-  'markdown': new MarkdownProvider(),
-  'mdx': new MdxProvider(),
+  // 'github': new GithubProvider(),
+  // 'markdown': new MarkdownProvider(),
+  // 'mdx': new MdxProvider(),
 } as const satisfies { [key: string]: Provider };
 
 export async function getProviders(){
